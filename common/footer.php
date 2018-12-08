186360791ac095304de433b96eea621fc35e983a
danishasbn <soobhendanisha@gmail.com>
2018-12-08 21:51:01 +0400

    Folder Structure
    Bootstrap JS & CSS included
    Icons on title tab set

---
+++ common/footer.php
@@ -0,0 +1,6 @@
+<!-- Bootstrap Main Scripts -->
+<script src="../Asset/js/jquery-3.3.1.slim.min.js"></script>
+<script src="../Asset/js/popper.min.js"></script>
+<script src="../Asset/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ common/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <!-- Title on the tab -->
+  <title>My Learning Platform</title>
+  <!-- Title tab icon -->
+  <link href="../Asset/images/icons/title-tab-icon.png" rel="icon"/>
+  <!-- Bootstrap 4.0.0 Main CSS  -->
+  <link href="../Asset/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
